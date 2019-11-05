d1cfe82c3c835f13b4a6c470250d795585637c2a
xandecoelho5 <xandecoelho5@hotmail.com>
2019-11-04 23:37:36 -0200

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO categoria (descricao) VALUES ('Informatica');
+INSERT INTO categoria (descricao) VALUES ('Eletronico');
+INSERT INTO categoria (descricao) VALUES ('Telefonia');
+INSERT INTO produto (nome, descricao, valor, categoria_id) VALUES ('Teclado Microsoft 3000', 'Teclado com tecnologia Wireless; ABNT2 ...', 199.49, 1);
+INSERT INTO produto (nome, descricao, valor, categoria_id) values ('Monitor 24pol. FHD Samsung', 'Monitor Full HD de 24pol. Taxa de atualização 144Hz...', 2099.00, 1);
+INSERT INTO produto (nome, descricao, valor, categoria_id) VALUES ('Smarpthone Samsung A9', 'Smartphone Samsung A9, Tela 2k 440ppi, 64GB ...', 199.49, 3);
+INSERT INTO produto (nome, descricao, valor, categoria_id) VALUES ('Home Theater LG LHB655NW', 'O novo Home Theater LG LHB655NW possui 5.1 canais de áudio e 1000W RMS de potencia,...', 1899.99, 2);
+INSERT INTO produto (nome, descricao, valor, categoria_id) VALUES ('Processador Core I9', 'Processador Intel Core I9 4.3Ghz...', 7099.99, 1);

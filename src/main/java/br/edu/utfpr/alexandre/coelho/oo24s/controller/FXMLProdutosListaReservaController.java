b649f0d7974797f050c5dc26cb40ad57ca675b2b
Alexandre Rodrigues <xandecoelho5@hotmail.com>
2019-12-10 01:42:18 -0300

    epa

--- src/main/java/br/edu/utfpr/alexandre/coelho/oo24s/controller/FXMLProdutosListaReservaController.java
+++ src/main/java/br/edu/utfpr/alexandre/coelho/oo24s/controller/FXMLProdutosListaReservaController.java
@@ -3,26 +3,21 @@ package br.edu.utfpr.alexandre.coelho.oo24s.controller;
 import br.edu.utfpr.alexandre.coelho.oo24s.dao.ProdutosDAO;
 import br.edu.utfpr.alexandre.coelho.oo24s.model.Produtos;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
-public class FXMLProdutosListaController implements Initializable {
+public class FXMLProdutosListaReservaController implements Initializable {
 
     @FXML
     private TableView<Produtos> tableData;
@@ -34,13 +29,19 @@ public class FXMLProdutosListaController implements Initializable {
     private TableColumn<Produtos, Double> columnValor;
     @FXML
     private TableColumn<Produtos, String> columnCategoria;
+    @FXML
+    private ChoiceBox cbProdutos;
+    @FXML
+    private TextField textTotal;
 
     private ProdutosDAO produtoDao;
     private ObservableList<Produtos> list = FXCollections.observableArrayList();
+    private List<Produtos> prodSelecionados;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         this.produtoDao = new ProdutosDAO();
+        this.prodSelecionados = new ArrayList<>();
         setColumnProperties();
         loadData();
     }
@@ -54,65 +55,29 @@ public class FXMLProdutosListaController implements Initializable {
 
     private void loadData() {
         list.clear();
-        list.addAll(produtoDao.getAll());
-
+        list.addAll(prodSelecionados);
         tableData.setItems(list);
     }
 
-    private void openForm(Produtos produto, ActionEvent event) {
-        try {
-            // Carregar o arquivo fxml e cria um
-            //novo stage para a janela Modal
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(this.getClass().getResource("/fxml/FXMLProdutosCadastro.fxml"));
-            AnchorPane pane = (AnchorPane) loader.load();
-
-            //Criando o stage para o modal
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("Cadastro de Produtos/Serviços");
-            dialogStage.initModality(Modality.WINDOW_MODAL);
-            dialogStage.initOwner(((Node) event.getSource()).getScene().getWindow());
-
-            Scene scene = new Scene(pane);
-            dialogStage.setScene(scene);
-
-            FXMLProdutosCadastroController controller = loader.getController();
-
-            controller.setProduto(produto);
-            controller.setDialogStage(dialogStage);
-            // Exibe a janela Modal e espera até o usuário
-            //fechar
-            dialogStage.showAndWait();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erro");
-            alert.setHeaderText("Ocorreu um erro ao abrir a janela de cadastro!");
-            alert.setContentText("Por favor, tente realizar a operação novamente!");
-            alert.showAndWait();
+    @FXML
+    private void addToList() {
+        Produtos produto = (Produtos) this.cbProdutos.getValue();
+        if (!prodSelecionados.contains(produto)) {
+            prodSelecionados.add(produto);
         }
         loadData();
+        //calcular total ao adicionar;
     }
 
     @FXML
-    private void edit(ActionEvent event) {
-        Produtos produto = tableData.getSelectionModel().getSelectedItem();
-        this.openForm(produto, event);
-    }
-
-    @FXML
-    private void newRecord(ActionEvent event) {
-        this.openForm(new Produtos(), event);
-    }
-
-    @FXML
-    private void delete(ActionEvent event) {
+    private void removeFromList() {
         if (tableData.getSelectionModel().getSelectedIndex() >= 0) {
             try {
                 Produtos produto = tableData.getSelectionModel().getSelectedItem();
+                prodSelecionados.remove(produto);
                 produtoDao.delete(produto.getId());
                 tableData.getItems().remove(tableData.getSelectionModel().getSelectedIndex());
+                //loadData();
             } catch (Exception e) {
                 e.printStackTrace();
                 Alert alert = new Alert(Alert.AlertType.ERROR);
--- src/main/java/br/edu/utfpr/alexandre/coelho/oo24s/controller/FXMLReservaManutController.java
+++ src/main/java/br/edu/utfpr/alexandre/coelho/oo24s/controller/FXMLReservaManutController.java
@@ -1,14 +1,107 @@
 package br.edu.utfpr.alexandre.coelho.oo24s.controller;
 
+import br.edu.utfpr.alexandre.coelho.oo24s.dao.ClienteDAO;
+import br.edu.utfpr.alexandre.coelho.oo24s.dao.HospedeDAO;
+import br.edu.utfpr.alexandre.coelho.oo24s.dao.QuartoDAO;
+import br.edu.utfpr.alexandre.coelho.oo24s.dao.ReservaDAO;
+import br.edu.utfpr.alexandre.coelho.oo24s.model.Cliente;
+import br.edu.utfpr.alexandre.coelho.oo24s.model.Hospede;
+import br.edu.utfpr.alexandre.coelho.oo24s.model.Quarto;
+import br.edu.utfpr.alexandre.coelho.oo24s.model.Reserva;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class FXMLReservaManutController implements Initializable {
 
+    @FXML
+    private VBox boxProdutos;
+    @FXML
+    private TextField textCliente;
+    @FXML
+    private TextField textQuarto;
+    @FXML
+    private ChoiceBox cbCliente;
+    @FXML
+    private TextField textValorDiaria;
+    @FXML
+    private ListView listHospedes;
+    @FXML
+    private TextField dateReserva;
+    @FXML
+    private TextField dateStart;
+    @FXML
+    private TextField dateEnd;
+    @FXML
+    private Button buttonAddToList;
+    @FXML
+    private TextField textActualUser;
+    @FXML
+    private TextField textUserRegistered;
+    @FXML
+    private ProgressBar progressReserva;
+    
+    private ReservaDAO reservaDAO;
+    private Stage stage;
+    private Reserva reserva;
+    private ClienteDAO clienteDAO;
+    private HospedeDAO hospedeDAO;
+    private QuartoDAO quartoDAO;
+    private List<Hospede> listaSelecionados;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        this.reserva = new Reserva();
+        this.reservaDAO = new ReservaDAO();
+        this.clienteDAO = new ClienteDAO();
+        this.hospedeDAO = new HospedeDAO();
+        this.quartoDAO = new QuartoDAO();
         
-    }    
+        //SET TEXTFIELD VALUES
+        
+        ObservableList<Cliente> clientes = FXCollections.observableArrayList(clienteDAO.getAll());
+        ObservableList<Hospede> hospedes = FXCollections.observableArrayList(hospedeDAO.getAll());
+
+        this.cbCliente.setItems(clientes);
+        this.listHospedes.setItems(hospedes); //GET ALL WITH WHERE RESERVA_ID = RESERVA_ID;
+    }  
     
+    public void setDataPane(Node node) {
+        boxProdutos.getChildren().setAll(node);
+    }
+
+    public VBox openVBox(String url) throws IOException {
+        VBox v = (VBox) FXMLLoader.load(this.getClass().getResource(url));            
+        FadeTransition ft = new FadeTransition(Duration.millis(1000));                
+        ft.setNode(v);
+        ft.setFromValue(0.1);
+        ft.setToValue(1);
+        ft.setCycleCount(1);
+        ft.setAutoReverse(false);
+        ft.play();
+        return v;
+    }
+    
+    private void confirmCliente() {
+        //SETPANE(boxProdutos);
+        //change all the fuckin things
+    }
 }
